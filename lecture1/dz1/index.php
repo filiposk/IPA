4cb7c28d167210937e6f29efeb6f5583331f10ae
filiposk <fpavlovic1995@gmail.com>
2019-01-16 23:31:54 +0100

    added css for bootstrap, created part of template

---
+++ lecture1/dz1/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <?php include_once "template/head.php" ?>
+</head>
+
+<body>
+
+<ul>
+    <li><a href="#home">Home</a></li>
+    <li><a href="bootsrap_page.php">Bootstrap</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#about">About</a></li>
+</ul>
+
+
+</body>
+</html>
